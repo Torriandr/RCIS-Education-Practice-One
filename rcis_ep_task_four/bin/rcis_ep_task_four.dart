a97ca0d9fc7f25dea7d1d46eed92bc28c667d25e
Torriandr <115963943+Torriandr@users.noreply.github.com>
2022-10-22 20:04:00 +0700

    Update rcis_ep_task_four.dart

--- rcis_ep_task_four/bin/rcis_ep_task_four.dart
+++ rcis_ep_task_four/bin/rcis_ep_task_four.dart
@@ -3,21 +3,24 @@ import 'dart:math';
 
 void main() {
   //zadanie 4 pr 1.1
-  List<int> f = [];
+  try {
+    stdout.write('Введите начало диапазона: ');
+    int a = int.parse(stdin.readLineSync()!);
+    stdout.write('Введите конец диапазона: ');
+    int b = int.parse(stdin.readLineSync()!);
+  
+    List<int> f = g(a,b);
 
-  stdout.write('Введите начало диапазона: ');
-  int a = int.parse(stdin.readLineSync()!);
-  stdout.write('Введите конец диапазона: ');
-  int b = int.parse(stdin.readLineSync()!);
-
-  List<int> fRandom(f) {
-
-    for (int i = 0; i < 10; i++) {
-      int n = Random().nextInt(b - a) + a;
-      f.add(n);
+    for (int i = 0; i < f.length; i++) {
+      stdout.write('${f[i]} ');
     }
-    return f;
   }
-  f = fRandom(f);
-  print(f.join(' '));
+  catch(e) {
+    print('Не число!');
+  }
+}
+List<int> g(int a, int b) {
+  List<int> d = List.generate(10, (i) => Random().nextInt(b - a) + a);
+    
+  return d;
 }
